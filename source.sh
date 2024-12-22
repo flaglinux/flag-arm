28b4ce9514eb139c3b14e92a1e44dff7777dc000
Sundew Shin <sundew.shin@clutchanalytics.com>
2024-12-21 22:18:30 -0600

    updated to mv the built img

--- source.sh
+++ source.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+#envsetup
+export PROJ=flag-arm
+export TINA_TOP=$(pwd)
+export VARIANT_CHOICES=tina
+
+#lunch
+export TINA_BUILD_TOP=${TINA_TOP}
+export TINA_TARGET_ARCH=arm
+export TARGET_PRODUCT=v536_Lindenis_SOM
+export TARGET_PLATFORM=v536
+export TARGET_BOARD=v536-Lindenis_SOM
+export TARGET_PLAN=Lindenis_SOM
+export TARGET_BUILD_VARIANT=tina
+export TARGET_BUILD_TYPE=release
+export TARGET_KERNEL_VERSION=4.9
+export TARGET_UBOOT=u-boot-2014.07
+export TARGET_CHIP=sun8iw16p1
+export LICHEE_PACK_OUT_DIR=${TINA_BUILD_TOP}/out/${TARGET_BOARD}/image
+export LICHEE_CHIP_CONFIG_DIR=${TINA_BUILD_TOP}/device/config/chips/${TARGET_PLATFORM}
+export BUILD_ENV_SEQUENCE_NUMBER=10
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
 make(){
     local start_time=$(date +"%s")
     local end_time=$(date +"%s")
@@ -29,47 +51,25 @@ make(){
 
 
 pack(){
-  ${TINA_TOP}/scripts/pack_img.sh \
+  local target_img="${TINA_TOP}/out/v536-Lindenis_SOM/tina_v536-Lindenis_SOM_uart0_nor.img"
+  rm ${target_img}
+
+  "${TINA_TOP}/scripts/pack_img.sh" \
     -b v536-Lindenis_SOM \
     -t ${TINA_TOP} \
     -c sun8iw16p1 \
     -m normal \
     -d uart0 \
-    -p tina #\
-#  -s none \
-#  -w none \
-#  -v none \
-#  -i none
+    -p tina
 
-  if [ $? -eq 0 ]; then
-    mv ${TINA_TOP}/out/v536-Lindenis_SOM/ ./flag-arm_$(git -C ${PROJ} log -n 1 --pretty=format:"%h").img
+  if [ -f "${target_img}" ]; then
+    mv "${target_img}" "./flag-arm_$(git log -n 1 --pretty=format:"%h").img"
     echo "Image packing completed successfully."
   fi
 }
 
 
 # MAIN
-#envsetup
-PROJ=flag-arm
-export TINA_TOP=$(pwd)
-export VARIANT_CHOICES=tina
-
-#lunch
-export TINA_BUILD_TOP=${TINA_TOP}
-export TINA_TARGET_ARCH=arm
-export TARGET_PRODUCT=v536_Lindenis_SOM
-export TARGET_PLATFORM=v536
-export TARGET_BOARD=v536-Lindenis_SOM
-export TARGET_PLAN=Lindenis_SOM
-export TARGET_BUILD_VARIANT=tina
-export TARGET_BUILD_TYPE=release
-export TARGET_KERNEL_VERSION=4.9
-export TARGET_UBOOT=u-boot-2014.07
-export TARGET_CHIP=sun8iw16p1
-export LICHEE_PACK_OUT_DIR=${TINA_BUILD_TOP}/out/${TARGET_BOARD}/image
-export LICHEE_CHIP_CONFIG_DIR=${TINA_BUILD_TOP}/device/config/chips/${TARGET_PLATFORM}
-export BUILD_ENV_SEQUENCE_NUMBER=10
-export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 rm -rf ${TINA_TOP}/tmp
 
 # merge split files
